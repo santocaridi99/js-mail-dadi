a9fd2dd908485b32ca41d48db3ec110d9e627dbc
santocaridi99 <santocaridi99@gmail.com>
2021-12-13 16:03:57 +0100

    finito esercizio dadi

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ flag = false;
 //creo un for  dove l' indice scorrerà tutta la lista
 //se l'email corrisponde allora il valore del flag diventerà vero
 for(let i=0 ; i < mail.length ; i++){
-    if(userMail === mail[i]){
+    if(userMail.toLowerCase() === mail[i].toLowerCase()){
         flag = true;
     }
 }
@@ -22,5 +22,27 @@ if(flag){
     console.log('Attenzione! email non corretta')
     htmlElement.innerHTML+='<div>Attenzione! email non corretta</div>';
 }
-
 //-----------------------------------------------------------
+//creo l'array dado contenente 6 numeri
+const dado = ['1','2','3','4','5','6'];
+let htmlElement2 = document.querySelector('.dadi');
+//creo due variabili random e uso la funzione math
+//es. const randomElement = array[Math.floor(Math.random() * array.length)];
+const utente = dado[Math.floor(Math.random() * dado.length)];
+console.log(utente);
+htmlElement2.innerHTML=`<div>utente: ${utente}</div>`;
+const computer = dado[Math.floor(Math.random() * dado.length)];
+console.log(computer);
+htmlElement2.innerHTML+=`<div>utente: ${computer}</div>`;
+//poi eseguo un controllo ovvero se(if) l'elemento randomgenerato1 è più grande di elemento generato2
+//stampare vince(quello con elemento randomgenerato1) altrimenti stampa che vince l'altro
+if(utente > computer){
+    console.log('Hai vinto!');
+    htmlElement2.innerHTML+=`<div>Hai Vinto :D</div>`;
+}else if(utente < computer){
+    console.log('oh noo il computer ha vinto');
+    htmlElement2.innerHTML+=`<div>oh noo il computer ha vinto!</div>`;
+}else{
+    console.log('incredibile pareggio');
+    htmlElement2.innerHTML+=`<div>incredibile pareggio</div>`;
+}
