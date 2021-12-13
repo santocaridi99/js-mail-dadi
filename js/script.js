d080b1646cd019085917448d5c5bafe5f2841ece
santocaridi99 <santocaridi99@gmail.com>
2021-12-13 15:09:35 +0100

    creazione file e aggiunta commenti per risolvere primo esercizio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//creo una lista di email 
+
+//prompt dove chiedo all' utente di inserire la propria email
+
+//dichiaro un flag in false che in caso l'utente sarà presente in true
+
+
+//creo un for  dove l' indice scorrerà tutta la lista
+//se l'email corrisponde allora il valore del flag diventerà vero
+
+
+//if se il valore del flag è vero , stampare "Benvenuto nel nostro sito"
+//altrimenti stampare "Attenzione! email non corretta"
